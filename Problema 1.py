3ff887769c1ff8bd2af2634ab420a4bc9a4b155c
IaroslavaDubitkaia <70658590+IaroslavaDubitkaia@users.noreply.github.com>
2020-11-11 10:13:51 +0300

    Add files via upload

---
+++ Problema 1.py
@@ -0,0 +1,9 @@
+n=int(input('Inroduceti un numar natural de la 28 la 31: '))
+if (n==31):
+    print("ianuarie, martie, mai, iulie, august, octombrie, decembrie")
+if (n==30):
+    print("aprilie, iunie, sepembrie, noiembrie")
+if (n==29):
+    print("februarie, an bisect")
+if (n==28):
+    print("februarie")   
